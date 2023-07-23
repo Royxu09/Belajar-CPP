1f3d5a6122b20bc16ec3e16c3835e7c590933725
Royxu09 <140304068+Royxu09@users.noreply.github.com>
2023-07-23 14:47:00 +0700

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    // untuk mencetak tulisan atau teks ke program
+
+    cout<<"Hello, Earth";
+
+    // fungsi pause adalah memberhentikan program saat program selesai dijalankan
+    system("pause");
+}
