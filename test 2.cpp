bd9c46cb72ab03e357f693ca0903f00c4dc00391
Royxu09 <140304068+Royxu09@users.noreply.github.com>
2023-07-23 15:19:10 +0700

    Update test 2.cpp

--- test 2.cpp
+++ test 2.cpp
@@ -3,14 +3,17 @@
 
 using namespace std;
 int main() {
+    //menentukan/mendeklarasikan nama dan umur (string= umum, int(angka/desimal/bil. bulat))
     string name;
     int umur;
-
+    
+    // memasukan data yang di imput dari user ke variabel yang telah di deklarasikan 
     cout<< "name";
     cin>> name;
-
     cout<< "umur";
     cin>> umur;
+
+    // mencetak/memunculkan data yang telah di masukan oleh user
     cout<<"name anda adalah "<<name<<"\n";
     cout<<"umur anda adalah "<<umur<<" tahun";
     system("pause");
