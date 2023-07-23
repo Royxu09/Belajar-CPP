7b48226431b0a2afcfc64a8e2da101d00b8ccb34
Royxu09 <140304068+Royxu09@users.noreply.github.com>
2023-07-23 15:15:11 +0700

    Add files via upload

---
+++ test 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+int main() {
+    string name;
+    int umur;
+
+    cout<< "name";
+    cin>> name;
+
+    cout<< "umur";
+    cin>> umur;
+    cout<<"name anda adalah "<<name<<"\n";
+    cout<<"umur anda adalah "<<umur<<" tahun";
+    system("pause");
+
+}
