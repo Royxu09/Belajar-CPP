0737a426b283591184664d20eee4d13447087fee
Royxu09 <140304068+Royxu09@users.noreply.github.com>
2023-07-24 17:15:23 +0700

    Update test 6.cpp

--- test 6.cpp
+++ test 6.cpp
@@ -11,8 +11,10 @@ int main(){
 
     int arr[data1][data2];
         int i,j;
+        // untuk mengulang data I/J=0 sampe dengan data i/J</=data1/2
+        // i itu bagian menurun pada suku pertama sedangkan j itu mendatar
         for (i=0; i<data1; i++ ){
-            for (j=0; j<data2; j++){
+            for (j=0; j<data2; j++){                                                          
                 cout<<"Silahkan input data matriks yang anda inginkan ["<<i<<"]"<<"["<<j<<"]";
                 cin>>arr[i][j]; 
             } 
